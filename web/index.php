3f565c5a6162e6b5c9ecad06790d8bea636a556f
isolev <72314749+isolev@users.noreply.github.com>
2020-10-03 21:01:51 +0300

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,39 +1,10 @@
-<?php
-    $srv = $_SERVER;
-
-    $cities = [
-        "Burgas" => 203017, 
-        "Dobrich" => 86292,
-        "Pleven" => 99628, 
-        "Plovdiv" => 341625,
-        "Ruse" => 145765, 
-        "Sliven" => 87895, 
-        "Sofia" => 1307376,
-        "Stara Zagora" => 148443,
-        "Varna" => 334466,
-        "Veliko Tarnovo" => 73508
-    ];
-
-    arsort($cities);
-?>
 <html>
     <head>
-        <title>Top cities in Bulgaria</title>
+        <title>My SoftUni ID</title>
     </head>
     <body>
-        <h3>Top cities in Bulgaria</h3>
-        <br />
-        <table>
-<?php
-    foreach ($cities as $c => $p) {
-        print "<tr>\n";
-        print "<td>$c</td>\n";
-        print "<td>$p</td>\n";
-        print "</tr>\n";
-    }
-?>
-        </table>
-        <br /><br /><br />
-        Running on <b><?php echo gethostname(); ?></b>
+        <h3>My SiftUni ID is: 300090269</h3>
+     
+        
     </body>
 </html>
